764fe08af335125674e89afbe0e5fdd3d4bf4671
David <david.garcia@lumation.co>
2021-07-04 20:01:39 -0600

    route added

--- ng-angular/src/app/app-routing.module.ts
+++ ng-angular/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeAdminComponent } from './components/home-admin/home-admin.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'home-admin', component: HomeAdminComponent},
   {path: 'login', component: LoginComponent},
+  {path: 'usuarios', component: UsuariosComponent},
   {path: '**', component: LoginComponent}
 ];
 
