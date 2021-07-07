5aa73544198c7624ab6809443be3685ad43f5b3f
Gabriel <gfigueros-2016113@kinal.edu.gt>
2021-07-06 22:37:11 -0600

    crud team

--- node/src/routes/equipo.routes.js
+++ node/src/routes/equipo.routes.js
@@ -5,6 +5,9 @@ const md_autenticacion = require('../middlewares/authenticated')
 
 var api = express.Router()
 
+api.put('/editarEquipo/:equipoID', md_autenticacion.ensureAuth, equipoController.editarEquipo);
 api.post('/registrarEquipo/:ligaID',  equipoController.registrarEquipo);
-
+api.get('/obtenerEquipoID/:equipoID', md_autenticacion.ensureAuth, equipoController.obtenerEquipoID);
+api.get('/obtenerEquipos', equipoController.obtenerEquipos);
+api.delete('/eliminarEquipo/:equipoID', md_autenticacion.ensureAuth, equipoController.eliminarEquipo);
 module.exports = api
