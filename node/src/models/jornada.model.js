9f0b01d2fb2caf1fa1f4575ec9242933d0272b0e
Jonatan W <josuejonatan142@gmail.com>
2021-07-08 05:29:55 -0600

    4 Jornadas

--- node/src/models/jornada.model.js
+++ node/src/models/jornada.model.js
@@ -8,8 +8,10 @@ var JornadaSchema = Schema({
     games: [{
         equipo1: { type: Schema.Types.ObjectId, ref: 'equipo'},
         nombre1: String,
+        imagen1: String,
         equipo2: { type: Schema.Types.ObjectId, ref: 'equipo'},
         nombre2: String,
+        imagen2: String,
         goles1: Number,
         goles2: Number,
     }]
